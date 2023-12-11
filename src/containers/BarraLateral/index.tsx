c10d250e1e2952929b5de78d523bc4136d6097cf
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-12-11 14:40:04 -0300

    criando a barra lateral e estilizando

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,5 +1,23 @@
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
 const BarraLateral = () => {
-  return <aside>barra lateral</aside>
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo type="text" placeholder="Buscar" />
+        <S.Filtros>
+          <FiltroCard contador={4} legenda="pendentes" />
+          <FiltroCard contador={2} legenda="concluidas" />
+          <FiltroCard contador={3} legenda="importantes" />
+          <FiltroCard contador={6} legenda="pendentes" />
+          <FiltroCard contador={1} legenda="urgentes" />
+          <FiltroCard ativo contador={3} legenda="concluidas" />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
 }
 
 export default BarraLateral
